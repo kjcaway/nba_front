1c2d4f8fe5747d706e4c71fbe68b8d7460726f1c
kjcaway <whdckstngh@naver.com>
2019-01-19 16:54:27 +0900

    set proxy using setupProxy.js

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Search.module.css'
+import axios from 'axios';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -8,17 +9,28 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons'
 library.add(faSearch)
 
 class Search extends React.Component {
-    constructor(props){
+    constructor(props) {
         super();
     }
 
-    render(){
+
+    /** test axios and proxy */
+    clickSearchBtn = async () => {
+        try {
+            const response = await axios.get('/api/');
+            console.log(response.data)
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    render() {
         return (
             <div className={styles.wrap}>
                 <div className={styles.search}>
-                    <input type="text" className={styles.searchTerm} placeholder="Write player name"/>
-                    <button type="submit" className={styles.searchButton}>
-                    <FontAwesomeIcon icon="search" />
+                    <input type="text" className={styles.searchTerm} placeholder="Write player name" />
+                    <button type="submit" className={styles.searchButton} onClick={this.clickSearchBtn.bind(this)}>
+                        <FontAwesomeIcon icon="search" />
                     </button>
                 </div>
             </div>
